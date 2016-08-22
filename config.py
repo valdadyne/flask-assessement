b5166bdc0bb429397b5eaeacd5e9145a1bdcfcec
valdadyne <rodgers.gitau@gmail.com>
2016-08-22 11:30:50 +0300

    Added Registration Details

---
+++ config.py
@@ -0,0 +1,5 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'cannot-touch-this'
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/communityapp'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
--- views.py
+++ views.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, flash, render_template, redirect,request,url_for
+from forms import Register
+from app import app,db,session
 
 app = Flask(__name__)
 
@@ -13,10 +15,16 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/register')
-def login():
-    return render_template('register.html')
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = Register(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User(form.username.data, form.email.data,
+                    form.password.data)
+        db_session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
 if __name__ == '__main__':
-    app.secret_key = 'mysecret'
     app.run(debug=True)
